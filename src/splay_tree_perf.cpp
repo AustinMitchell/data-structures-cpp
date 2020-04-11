b3431c362d1fa402b223589574422935129a42da
Austin <austin.mitchell@gmail.com>
2020-04-10 21:00:47 -0400

    Added an extra test to splay_tree_test, added the beginning of performance measurement code

--- src/splay_tree_perf.cpp
+++ src/splay_tree_perf.cpp
@@ -4,10 +4,63 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <random>
 #include <chrono>
 
-#include "dsc/splay_tree.hpp"
+#include <dsc/splay_tree.hpp>
 
-int main(int argc, char *argv[]) {
+using std::cout;
+using timer = std::chrono::high_resolution_clock;
 
+auto const NUM_VALUES = 1000000;
+auto tree_full  = dsc::splay_tree<int>{};
+auto tree_semi  = dsc::semisplay_tree<int>{};
+
+
+auto construct_trees() {
+    auto nums       = std::vector<int>{};
+    nums.reserve(NUM_VALUES);
+    for (auto i=1; i<=NUM_VALUES; i++) {
+        nums.push_back(i);
+    }
+
+    //  to reduce initialization time, create initially balanced trees.
+    tree_full = dsc::splay_tree<int>{nums};
+    tree_semi = dsc::semisplay_tree<int>{nums};
+}
+
+
+auto main() -> int {
+    auto rd             = std::random_device{};
+    auto gen            = std::mt19937 {rd()};
+    auto next_uniform   = std::uniform_int_distribution<>(1, NUM_VALUES);
+
+    construct_trees();
+
+    cout << "Deleting nodes in order on full splay tree\n";
+    {
+        auto prev       = tree_full.delete_min_no_splay();
+        auto current    = tree_full.delete_min_no_splay();
+        auto start      = timer::now();
+        while (!tree_full.empty()) {
+            prev    = current;
+            current = tree_full.delete_min_no_splay();
+        }
+        auto end        = timer::now();
+        cout << "Elapsed time: " << (std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()/1000.0) << "\n";
+    }
+
+    cout << "\n";
+    cout << "Deleting nodes in order on semi splay tree\n";
+    {
+        auto prev       = tree_semi.delete_min_no_splay();
+        auto current    = tree_semi.delete_min_no_splay();
+        auto start      = timer::now();
+        while (!tree_semi.empty()) {
+            prev    = current;
+            current = tree_semi.delete_min_no_splay();
+        }
+        auto end        = timer::now();
+        cout << "Elapsed time: " << (std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count()/1000.0) << "\n";
+    }
 }
--- src/splay_tree_test.cpp
+++ src/splay_tree_test.cpp
@@ -9,6 +9,8 @@
 #include "dsc/splay_tree.hpp"
 #include "dsc/tree_printer.hpp"
 
+using std::cout;
+
 int main(int argc, char *argv[]) {
     std::srand(static_cast<long unsigned int>(std::chrono::high_resolution_clock::now().time_since_epoch().count()));
 
@@ -16,46 +18,55 @@ int main(int argc, char *argv[]) {
     auto tree2 = dsc::semisplay_tree<int>{};
     auto list = std::vector<int>{};
 
-    int size = argc >= 2 ? std::atoi(argv[1]) : 20;
-
-    list.reserve(size);
-    for (int i=1; i<=size; i++) {
+    list.reserve(20);
+    for (int i=1; i<=20; i++) {
         list.push_back(i);
     }
 
     std::random_shuffle(list.begin(), list.end());
 
     for (auto i: list) {
-        //std::cout << "inserting " << list[i] << "...\n";
+        //cout << "inserting " << list[i] << "...\n";
         tree1.insert(i);
         tree2.insert(i);
         //print_ascii_tree(tree.root());
     }
 
-    std::cout << "Final splay tree:\n";
+    cout << "Final splay tree:\n";
     dsc::print_ascii_tree(tree1.root());
-    std::cout << "\n";
-    std::cout << "Final semisplay tree:\n";
+    cout << "\n";
+    cout << "Final semisplay tree:\n";
     dsc::print_ascii_tree(tree2.root());
 
-    std::cout << "\n";
-    std::cout << "Insert order:                  ";
+    cout << "\n";
+    cout << "Insert order:                  ";
     for(auto v: list) {
-        std::cout << v << " ";
+        cout << v << " ";
     }
 
-    std::cout << "\n";
-    std::cout << "In-order traversal full splay: ";
+    cout << "\n";
+    cout << "In-order traversal full splay: ";
     for(auto const& v: tree1) {
-        std::cout << v << " ";
+        cout << v << " ";
     }
 
-    std::cout << "\n";
-    std::cout << "In-order traversal semi splay: ";
+    cout << "\n";
+    cout << "In-order traversal semi splay: ";
     for(auto const& v: tree2) {
-        std::cout << v << " ";
+        cout << v << " ";
     }
-    std::cout << "\n";
+    cout << "\n";
+
+    cout << "Creating a balanced tree with vector constructor\n";
+    auto sorted = std::vector<int>{};
+    auto size   = argc >= 2 ? std::atoi(argv[1]) : 15;
+    for (auto i=1; i<=size; i++) {
+        sorted.push_back(i);
+    }
+    auto balanced_tree = dsc::splay_tree<int>{sorted};
+
+    cout << "Balanced splay tree:\n";
+    dsc::print_ascii_tree(balanced_tree.root());
 
     return 0;
 }
