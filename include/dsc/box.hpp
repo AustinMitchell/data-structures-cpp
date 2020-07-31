913b9d7cf192799378de53dce96b3c2033fdbd9f
Austin <austin.mitchell@gmail.com>
2020-07-30 22:00:40 -0400

    Added -pthread to linker flags. Finished box, no longer throws system error.

--- include/dsc/box.hpp
+++ include/dsc/box.hpp
@@ -13,37 +13,45 @@ class box {
     T                       data_;
     bool                    has_data_;
     std::mutex              mutex_;
-    std::condition_variable cv_;
+    std::condition_variable get_cv_, put_cv_;
 
  public:
-    box(): data_(), mutex_() {}
-    explicit box(T init_data): data_(std::move(init_data)), mutex_() {}
+    box():  data_(),
+            has_data_(false),
+            mutex_() {}
+
+    explicit box(T init_data):  data_(std::move(init_data)),
+                                has_data_(false),
+                                mutex_() {}
 
     auto get() -> T {
-        auto lock = std::unique_lock{mutex_, std::defer_lock};
+        auto lock = std::unique_lock{mutex_};
+
         while (true) {
-            if (has_data_ && lock.try_lock()) {
+            if (has_data_) {
                 has_data_ = false;
                 auto ret = T{std::move(data_)};
-                cv_.notify_all();
+                put_cv_.notify_all();
                 return ret;
             }
 
-            cv_.wait(lock);
+            get_cv_.wait(lock);
         }
     }
 
     auto put(T new_data) -> void {
-        auto lock = std::unique_lock{mutex_, std::defer_lock};
+        auto lock = std::unique_lock{mutex_};
+
         while (true) {
-            if (!has_data_ && lock.try_lock()) {
+            if (!has_data_) {
                 has_data_ = true;
                 data_ = std::move(new_data);
-                cv_.notify_all();
+                get_cv_.notify_all();
                 return;
             }
 
-            cv_.wait(lock);
+            // note: wait() will unlock and then reacquire lock after being notified
+            put_cv_.wait(lock);
         }
     }
 };
--- src/box_test.cpp
+++ src/box_test.cpp
@@ -2,34 +2,51 @@
 
 #include <iostream>
 #include <thread>
+#include <mutex>
 #include "dsc/box.hpp"
 
 using std::cout;
 
-void getter(dsc::box<int>& box, int idx) {
-    for (int i=0; i<10; i++) {
+std::mutex cout_mutex;
+
+void getter(dsc::box<int>& box, int idx, int rounds) {
+    {
+        auto lock = std::lock_guard{cout_mutex};
+        cout << "Getter " << idx << " started\n";
+    }
+    for (int i=0; i<rounds; i++) {
         auto v = box.get();
-        cout << "Getter " << idx << " recieved " << v << "\n";
+        {
+            auto lock = std::lock_guard{cout_mutex};
+            cout << "Getter " << idx << " recieved " << v << "\n";
+        }
     }
 }
 
-void putter(dsc::box<int>& box, int idx) {
-    for (int i=0; i<10; i++) {
+void putter(dsc::box<int>& box, int idx, int rounds) {
+    {
+        auto lock = std::lock_guard{cout_mutex};
+        cout << "Setter " << idx << " started\n";
+    }
+    for (int i=0; i<rounds; i++) {
         box.put(idx);
-        cout << "Setter " << idx << " placed value\n";
+        {
+            auto lock = std::lock_guard{cout_mutex};
+            cout << "Setter " << idx << " placed value\n";
+        }
     }
 }
 
 int main() {
     auto box = dsc::box<int>{};
 
-    auto getter1 = std::thread{getter, box, 1};
-    auto getter2 = std::thread{getter, box, 2};
-    auto getter3 = std::thread{getter, box, 3};
+    auto getter1 = std::thread{getter, std::ref(box), 1, 5};
+    auto getter2 = std::thread{getter, std::ref(box), 2, 5};
+    auto getter3 = std::thread{getter, std::ref(box), 3, 5};
 
-    auto putter1 = std::thread{putter, box, 1};
-    auto putter2 = std::thread{putter, box, 2};
-    auto putter3 = std::thread{putter, box, 3};
+    auto putter1 = std::thread{putter, std::ref(box), 1, 5};
+    auto putter2 = std::thread{putter, std::ref(box), 2, 5};
+    auto putter3 = std::thread{putter, std::ref(box), 3, 5};
 
     getter1.join();
     getter2.join();
