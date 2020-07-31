3bd4e5a1b195592e3325727d7480934610564802
Austin <austin.mitchell@gmail.com>
2020-07-30 22:10:42 -0400

    Added some documentation and fixed small bug with init_data constructor

--- include/dsc/box.hpp
+++ include/dsc/box.hpp
@@ -8,6 +8,8 @@
 
 namespace dsc {
 
+
+/** Thread-safe storage object which can store a single item through get(), which can be retrieved with put(). */
 template<typename T>
 class box {
     T                       data_;
@@ -16,14 +18,17 @@ class box {
     std::condition_variable get_cv_, put_cv_;
 
  public:
+    /** Constructs an empty box. */
     box():  data_(),
             has_data_(false),
             mutex_() {}
 
+    /** Constructs a box with initial data. */
     explicit box(T init_data):  data_(std::move(init_data)),
-                                has_data_(false),
+                                has_data_(true),
                                 mutex_() {}
 
+    /** Retrieves item from box. If there is no item, blocks until an item is placed inside. */
     auto get() -> T {
         auto lock = std::unique_lock{mutex_};
 
@@ -39,6 +44,7 @@ class box {
         }
     }
 
+    /** Places item within box. If there is already an item, blocks until the box is empty. */
     auto put(T new_data) -> void {
         auto lock = std::unique_lock{mutex_};
 
